f60d163df2b7ce59621396e34997f00e46c3a445
Nande <vicenteignacioac22@gmail.com>
2022-09-30 02:57:16 -0300

    tipos de datos basicos dart

---
+++ tiposdedatos.dart
@@ -0,0 +1,33 @@
+// Tipos de datos boolean
+
+/*void main() {
+  print("Programa para saber si esta soleado");
+  bool estaSoleado = false;
+  print(estaSoleado);
+}
+*/
+// Tipos de datos numericos
+/*void main() {
+  print("Programa para saber la edad y estatura");
+  int edad = 20;
+  print("Edad");
+  print(edad);
+  double estatura = 1.65;
+  print("Estatura");
+  print(estatura);
+}
+*/
+// Tipos de datos string
+/*void main() {
+  print("Programa para saber el nombre de un pais");
+  String pais = "Chile";
+  print("Nombre del pais: $pais");
+  print(pais);
+}
+*/
+// Tipos de datos colecciones [list]
+void main() {
+  print("Programa para saber los nombres de mis amigos");
+  List<String> amigos = ["Ignacio", "Camilo", "Nicoax"];
+  print("Amigos: $amigos");
+}
