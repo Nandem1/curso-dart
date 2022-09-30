b4036546a70961678793280dffef295e615874fe
Nande <vicenteignacioac22@gmail.com>
2022-09-30 03:30:30 -0300

    tipos de datos avanzados dart

---
+++ tiposdedatosavanzados.dart
@@ -0,0 +1,29 @@
+// Tipo de dato var
+/*void main() {
+  var nombre = "Sofia";
+  print(nombre);
+
+  nombre = "Vicente";
+  print(nombre);
+}
+*/
+// Tipo de dato final vs const
+/*void main() {
+  const nombre = "Sofia";
+  print(nombre);
+
+  final pi = 3.14;
+  print(pi);
+}
+*/
+// Tipo de dato dynamic
+void main() {
+  dynamic nombre = "Sofia";
+  print(nombre);
+
+  nombre = "Vicente";
+  print(nombre);
+
+  nombre = 3.14;
+  print(nombre);
+}
