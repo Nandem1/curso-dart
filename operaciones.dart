63624dca52f2034f4edcdcaec5793b0e0b9cd1ef
Nande <vicenteignacioac22@gmail.com>
2022-09-30 15:10:24 -0300

    operaciones dart

---
+++ operaciones.dart
@@ -0,0 +1,39 @@
+/*void main() {
+  // Operaciones Aritmeticas
+  // +, -, -expr, *, /, ~/, y %
+  int valor1 = 7;
+  int valor2 = 3;
+  int resultado = valor1 * valor2;
+  print("$valor1 * $valor2 = $resultado");
+}
+*/
+// Operaciones de Asignacion
+/*void main() {
+  int valor1 = 8;
+  int valor2 = 2;
+  print("$valor1 += $valor2");
+  valor1 += valor2;
+  print("$valor1");
+}
+*/
+// Operaciones Relacionales
+/*void main() {
+  // ==, =!, =>, >, < y <=
+  int valor1 = 3;
+  int valor2 = 3;
+
+  bool resultado = valor1 >= valor2;
+
+  print("$valor1 > $valor2 = $resultado");
+}
+*/
+//Operaciones Logicas
+void main() {
+  // ||, &&, !, ?:
+  bool llueve = true;
+  bool haceFrio = true;
+
+  bool llevoAbrigo = llueve && haceFrio;
+
+  print("$llueve && $haceFrio = $llevoAbrigo");
+}
