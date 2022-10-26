b2d4088526d76f890f60f46034ba8c1ba0835952
Nande <vicenteignacioac22@gmail.com>
2022-10-25 21:05:07 -0300

    chupalo

---
+++ retodecolecciones.dart
@@ -0,0 +1,29 @@
+void main() {
+  Set restaurantes = {
+
+{
+“nombre”: “El novillo alegre”,
+“tipo”: “Argentina”,
+“calificaciones”: [4, 5, 2, 1, 2]
+},
+
+{
+“nombre”: “Baires Grill”,
+“tipo”: “Argentina”,
+“calificaciones”: [3, 1, 3, 5, 5, 2, 3]
+},
+
+{
+“nombre”: “Mario Brothers”,
+“tipo”: “Italiana”,
+“calificaciones”: [4, 3, 2, 1, 1]
+},
+
+{
+“nombre”: “Molto bene”,
+“tipo”: “Italiana”,
+“calificaciones”: [4, 3, 1, 1, 5]
+}
+
+};
+}
